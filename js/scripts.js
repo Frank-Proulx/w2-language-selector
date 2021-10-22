ec77d1a5068bc0298a192ff16531378f60b1e6dc
Frank-Proulx <frankproulx7@gmail.com>
2021-10-22 10:26:46 -0700

    add business logic and if/else framework for ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ function average(num1, num2, num3, num4, num5) {
   return (num1 + num2 + num3 + num4 + num5) / 5;
 }
 
+
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     const question1 = parseInt($("#question1").val());
@@ -9,6 +10,14 @@ $(document).ready(function() {
     const question3 = parseInt($("#question3").val());
     const question4 = parseInt($("#question4").val());
     const question5 = parseInt($("#question5").val());
+    let result = average(question1, question2, question3, question4, question5);
+    if (result < (5 + 10/3)) {
+      return 1;
+    } else if (result >= (5 + 10/3) && result < (5 + 20/3)) {
+      return 2;
+    } else {
+      return 3;
+    }
     event.preventDefault();
 
   });
